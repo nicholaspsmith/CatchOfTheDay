2d67b275152e1c40a17e771c6173fb12e0402d63
Nicholas Smith <npsmith1990@gmail.com>
2016-04-01 19:32:10 -0400

    Add scripts

---
+++ scripts/main.js
@@ -0,0 +1,19 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+/*
+  StorePicker
+  Renders <StorePicker/>
+*/
+
+var StorePicker = React.createClass({
+
+  render: function() {
+    return (
+      <p>hello</p>
+    )
+  }
+
+});
+
+ReactDOM.render(<StorePicker/>, document.querySelector('#main'));
