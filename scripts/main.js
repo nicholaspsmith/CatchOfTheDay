e08982f842177b11ee07e5059f6f5525d5b5464b
Nicholas Smith <npsmith1990@gmail.com>
2016-04-13 12:57:22 -0500

    update styles of header

--- scripts/main.js
+++ scripts/main.js
@@ -22,7 +22,7 @@ var App = React.createClass({
     return (
       <div className="catch-of-the-day">
         <div className="menu">
-          <Header tagline="Fresh Seafood Stuff" />
+          <Header tagline="Fresh Seafood Market" />
         </div>
         <Order/>
         <Inventory/>
@@ -40,8 +40,13 @@ var Header = React.createClass({
   render: function() {
     return (
       <header className="top">
-        <h1>Catch of the Day</h1>
-        <h3 className="tagline">{this.props.tagline}</h3>
+        <h1>Catch
+        <span className="ofThe">
+          <span className="of">of</span>
+          <span className="the">the</span>
+        </span>
+        Day</h1>
+        <h3 className="tagline"><span>{this.props.tagline}</span></h3>
       </header>
     )
   }
