4a232d51de193c52cc23805b5e25acd9c2ef226a
Nicholas Smith <npsmith1990@gmail.com>
2016-04-14 18:22:10 -0400

    Add React router

--- scripts/main.js
+++ scripts/main.js
@@ -12,6 +12,12 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 
+var ReactRouter = require('react-router');
+var Router = ReactRouter.Router;
+var Route = ReactRouter.Route;
+var Navigation = ReactRouter.Navigation;
+var createBrowserHistory = require('history/lib/createBrowserHistory');
+
 /*
   App
   <App/>
@@ -98,4 +104,28 @@ var StorePicker = React.createClass({
 
 });
 
-ReactDOM.render(<App/>, document.querySelector('#main'));
+
+/*
+  Not Found
+*/
+
+var NotFound = React.createClass({
+  render: function() {
+    return <h1>Not found</h1>
+  }
+});
+
+/*
+  Routes
+*/
+
+var routes = (
+  <Router history={createBrowserHistory()}>
+    <Route path="/" component={StorePicker} />
+    <Route path="/store/:storeName" component={App} />
+    <Route path="*" component={NotFound} />
+  </Router>
+)
+
+
+ReactDOM.render(routes, document.querySelector('#main'));
