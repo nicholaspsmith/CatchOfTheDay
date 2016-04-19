b0ac438658de2ad699a85e7299e71090af05e495
Nicholas Smith <npsmith1990@gmail.com>
2016-04-19 08:22:10 -0400

    Render individual items in order

--- scripts/main.js
+++ scripts/main.js
@@ -161,28 +161,45 @@ var Header = React.createClass({
 */
 
 var Order = React.createClass({
+  renderOrder: function(key) {
+    var fish = this.props.fishes[key];
+    var count = this.props.order[key];
+
+    if (!fish) {
+      return <li key={key}>Sorry, fish is no longer available</li>
+    }
+
+    return (
+      <li>
+        <span>{count}</span>lbs
+        {fish.name}
+        <span className="price">{h.formatPrice(count * fish.price)}</span>
+      </li>
+    )
+  },
   render: function() {
     var orderIds = Object.keys(this.props.order);
+
     var total = orderIds.reduce((prevTotal, key) => {
       var fish = this.props.fishes[key];
       var count = this.props.order[key];
       var isAvailable = fish && fish.status === 'available';
-
-      if (fish && isAvailable) {
+      if (isAvailable) {
         return prevTotal + (count * parseInt(fish.price) || 0);
       }
-
       return prevTotal;
     }, 0);
+
     return (
       <div className="order-wrap">
         <h2 className="order-title">Your Order</h2>
-        <div className="order">
+        <ul className="order">
+          {orderIds.map(this.renderOrder)}
           <li className="total">
             <strong>Total:</strong>
             {h.formatPrice(total)}
           </li>
-        </div>
+        </ul>
       </div>
     )
   }
