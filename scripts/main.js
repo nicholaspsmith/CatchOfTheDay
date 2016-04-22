9b1f91b832bbbdc4dc18ade22b8eff19f82583e6
Nick P Smith <npsmith1990@gmail.com>
2016-04-22 08:20:56 -0600

    add ability to remove a menu item'

--- scripts/main.js
+++ scripts/main.js
@@ -68,6 +68,14 @@ var App = React.createClass({
     // set the state
     this.setState({ fishes: this.state.fishes });
   },
+  removeFish: function(key) {
+    if (confirm("Are you sure you want to remove this menu item?")) {  
+      this.state.fishes[key] = null;
+      this.setState({
+        fishes: this.state.fishes
+      });
+    }
+  },
   loadSamples: function() {
     this.setState({
       fishes: require('./sample-fishes')
@@ -86,7 +94,7 @@ var App = React.createClass({
           </ul>
         </div>
         <Order fishes={this.state.fishes} order={this.state.order} />
-        <Inventory addFish={this.addFish} loadSamples={this.loadSamples} fishes={this.state.fishes} linkState={this.linkState} />
+      <Inventory addFish={this.addFish} loadSamples={this.loadSamples} fishes={this.state.fishes} linkState={this.linkState} removeFish={this.removeFish} />
       </div>
     )
   }
@@ -249,7 +257,7 @@ var Inventory = React.createClass({
         </select>
         <textarea valueLink={linkState('fishes.' + key + '.desc')}></textarea>
         <input type="text" valueLink={linkState('fishes.' + key + '.image')} />
-        <button>Remove Fish</button>
+      <button onClick={this.props.removeFish.bind(null, key)}>Remove Fish</button>
       </div>
     )
   },
